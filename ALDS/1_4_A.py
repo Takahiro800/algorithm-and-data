a87ec9d29778eae64dd9537ae7efa29776562929
Takahiro Abe <hirotaka081630@gmail.com>
2020-11-03 21:05:17 +0900

    p119 Linear Search

---
+++ ALDS/1_4_A.py
@@ -0,0 +1,14 @@
+n = int(input())
+S = list(map(int, input().split()))
+q = int(input())
+T = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(q):
+  for j in range(n):
+    if T[i] == S[j]:
+      ans += 1
+      break
+
+print(ans)
